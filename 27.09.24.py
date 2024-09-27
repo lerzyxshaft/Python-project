55536d3272c1eb2d45c450996e1ebf42e20b283b
shaft <zxcshaft@gmail.com>
2024-09-27 23:08:41 +0300

    Guess number game

--- 27.09.24.py
+++ 27.09.24.py
@@ -33,3 +33,22 @@ while isHisCar:
         break
     else :
         print(question)
+
+for i in range(1, 11):
+    if i == 5:
+        break
+
+    if i % 2 == 0:
+        continue
+
+    print(i)
+
+
+found = None
+for i in "hello":
+    if i == "l":
+        found = True
+        break
+    else :
+        found = False
+print(found)
