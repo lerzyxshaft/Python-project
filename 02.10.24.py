571d2db6485eb3e8192c0951b7295090b18cb6c8
shaft <zxcshaft@gmail.com>
2024-10-03 00:21:08 +0300

    Guess number game

--- 02.10.24.py
+++ 02.10.24.py
@@ -1,17 +1,33 @@
 #8 python lesson
 
-word = 'shaft'
+word = 'shaft, skate, kebab'
 print(word[1])
 
-#як результат коду ми отритмаємо літеру 's', викликану функцією прінт на екран
+#як результат коду ми отримаємо літеру 's', викликану функцією прінт на екран
 
 print(len(word))
 print(word.count('a'))
 
 #ми можемо використовувати для змінної всі ті самі функції які використовуються для списку
 
-print(word.isupper())#print(word.islower())#
+print(word.isupper())
+print(word.islower())#опускає всі літери рядка
 print(word.istitle())#
-print(word.lower())#
-print(word.capitalize())#
-print(word.find(a))#
+print(word.lower())#понижує літери
+print(word.capitalize())#робить першу літеру рядка великою і понижує всі наступні
+print(word.find('a'))#знаходить поставлені в дужки символи з рядку
+
+hobby = word.split(',')#ця функція розділяє рядок на символ зазначений в дужках
+print(hobby[1])
+
+for i in  range(len(hobby)):
+    hobby[i] = hobby[i].capitalize()
+
+result = ', '.join(hobby)
+
+print(result)
+
+
+dan = 'Football'
+
+print(dan[4:8])
