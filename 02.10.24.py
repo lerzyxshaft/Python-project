d65824e007d0ec30438ede3ca567898342564de7
shaft <zxcshaft@gmail.com>
2024-10-02 23:52:33 +0300

    Guess number game

---
+++ 02.10.24.py
@@ -0,0 +1,17 @@
+#8 python lesson
+
+word = 'shaft'
+print(word[1])
+
+#як результат коду ми отритмаємо літеру 's', викликану функцією прінт на екран
+
+print(len(word))
+print(word.count('a'))
+
+#ми можемо використовувати для змінної всі ті самі функції які використовуються для списку
+
+print(word.isupper())#print(word.islower())#
+print(word.istitle())#
+print(word.lower())#
+print(word.capitalize())#
+print(word.find(a))#
