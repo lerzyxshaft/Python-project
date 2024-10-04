3b50055bd93cdf945fd88c3901d57d3047a890ae
shaft <zxcshaft@gmail.com>
2024-10-04 23:36:27 +0300

    Guess number game

--- ITprogtasks.py
+++ ITprogtasks.py
@@ -6,3 +6,18 @@ for elem in a:
         break
 
 #####04.10.24#####
+#tuples lesson
+data = (5, 6, 3, 4, True, 3.4, 'Hello')#you also can create tuples without brackets,
+# but you need to put ',' after element
+
+# #data[0] = 1 in tuples you can't change anything
+#print(data.count(60))
+#print(len(data))
+print(data[1])
+
+nums = [4, 56, 2, 463,]
+new_nums = tuple(nums)
+print(new_nums)
+
+word = tuple("hello world")
+print(word)
