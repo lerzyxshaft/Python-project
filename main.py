a13e5262084bcdbca4f7e07a751a6ecd6b7d5307
shaft <zxcshaft@gmail.com>
2024-11-16 00:43:54 +0200

    15Nov

---
+++ main.py
@@ -0,0 +1,18 @@
+class Cat:
+    name = None
+    age = None
+    isHappy = None
+
+    def set_data(self, name, age, isHappy):
+        self.name = name
+        self.age = age
+        self.isHappy = isHappy
+
+    def get_data(self):
+        print(self.name, 'age:', self.age, 'Happy:', self.isHappy)
+
+cat1 = Cat()
+cat1.set_data('barsik', 11, True)
+print(cat1)
+
+
